650cec75173177e55eec1be7c67e2785942f9ddd
habibabakr4 <habebabakr4@gmail.com>
2024-09-23 00:06:00 +0300

    add autherize_request to auth using token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+
+    def autherize_request
+        header=request.headers['Authorization']
+        token = header.split(' ').last if header
+        decoded = JwtService.decode(token)
+
+        @current_driver = Driver.find(decoded[:driver_id]) if decoded
+
+        rescue ActiveRecord::RecordNotFound, JWT::DecodeError
+            render json: { errors: 'Unauthorized' }, status: :unauthorized
+        end
+
+    end
 end
